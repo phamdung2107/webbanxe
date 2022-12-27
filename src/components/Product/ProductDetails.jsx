197cb408e4cca67f080d41a0a5121edb962cfd46
phamdung2107 <phamdung21072002@gmail.com>
2022-12-27 14:55:40 +0700

    push

--- src/components/Product/ProductDetails.jsx
+++ src/components/Product/ProductDetails.jsx
@@ -10,6 +10,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { Link } from "react-router-dom";
 
 const rows = [
   {
@@ -95,43 +96,72 @@ const ProductDetails = () => {
               <p className="mmr30">{product.description}</p>
             </div>
             <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              <div
-                className="pointer flex"
-                style={{
-                  padding: "10px 5px",
-                  alignItems: "center",
-                  backgroundColor: "#E4EAEC",
-                }}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  className="bi bi-bag"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                </svg>
-                <button
-                  className="cartBtn"
                   style={{
-                    opacity: 0.7,
-                    padding: "0px 5px",
-                    border: "none",
-                    cursor: "pointer",
-                    background: "none",
+                    display: "flex",
+                    alignItems: "center",
                   }}
                 >
-                  Xem sau
-                </button>
-              </div>
-            </div>
+                  <div
+                    className="wishlist"
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      cursor: "pointer",
+                      padding: "15px 5px",
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-heart"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
+                    </svg>
+                    <span
+                      className="cartBtn"
+                      style={{ opacity: 0.7, padding: "0px 5px" }}
+                    >
+                      Xem sau
+                    </span>
+                  </div>
+
+                  <div
+                    className="pointer flex"
+                    style={{
+                      padding: "10px 5px",
+                      alignItems: "center",
+                      backgroundColor: "#E4EAEC",
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-bag"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                    </svg>
+                    <Link to='/lichhen'>
+                      <button
+                        className="cartBtn"
+                        style={{
+                          opacity: 0.7,
+                          padding: "0px 5px",
+                          border: "none",
+                          cursor: "pointer",
+                          background: "none",
+                        }}
+                      >
+                        Đặt Lịch Xem Xe
+                      </button>
+                    </Link>
+                  </div>
+                </div>
           </div>
         </div>
       </div>
